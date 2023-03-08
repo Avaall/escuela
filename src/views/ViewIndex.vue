c3dac072d0ace0b7f5a53dc305843bc2e26e40a0
Mona Lisa <anfecilo@gmail.com>
2023-03-08 10:13:18 -0500

    responsive 1

--- src/views/ViewIndex.vue
+++ src/views/ViewIndex.vue
@@ -4,12 +4,12 @@
         <div class="home-header">
             <div class="home-header-bg align-items-center d-flex">
                 <div class="d-flex w-100 justify-content-between align-items-center">
-                    <div class="mx-5">
+                    <div class="home-header-texto">
                         <p class="home-header-texto-1 m-0">Alcaldía de Medellín</p>
                         <p class="home-header-texto-2 m-0">Escuela Virtual de Aprendizaje</p>
                     </div>
                     <div class="cursor" v-on:click="AbrirMenu">
-                        <img src="../assets/img/boton-ingreso.png" alt="" width="220px" class="mx-5">
+                        <img src="../assets/img/boton-ingreso.png" alt="" class="img-ingreso">
                     </div>
                 </div>
             </div>
@@ -64,11 +64,47 @@
                         <button v-on:click="cerrar(0)">Cerrar</button>
                     </div>
                 </div>
+                <div class="home-informacion-body-responsive">
+                    Es una de las estrategias de formación, capacitación y entrenamiento de
+                    la Alcaldía de Medellín para fortalecer las competencias laborales de los
+                    servidores públicos, mediante programas de aprendizaje con énfasis en
+                    actualización permanente del fortalecimiento de la cultura organizacional
+                    y la ética de los servicios ofrecidos a la ciudadanía.
+                </div>
+                <div class="home-informacion-body-responsive-md">
+                    Es una de las estrategias de formación, capacitación y entrenamiento de
+                    la Alcaldía de Medellín para fortalecer las competencias laborales de los
+                    servidores públicos, mediante programas de aprendizaje con énfasis en
+                    actualización permanente del fortalecimiento de la cultura organizacional
+                    y la ética de los servicios ofrecidos a la ciudadanía.
+                    <div>
+                        <button v-on:click="cerrar(0)">Cerrar</button>
+                    </div>
+                </div>
                 <div class="home-informacion-button" v-on:click="MostrarInfoButton(1)">
                     <img src="../assets/img/modal-icon-2.png" alt="" class="mt-2 home-icon-informacion">
                     <p class="text-center mx-2 home-valor-texto-titulo">¿Cuándo nace? <br> &nbsp;</p>
                 </div>
                 <div class="home-informacion-body">
+                    Se crea desde la Secretaría de Gestión Humana y Servicio a la Ciudadanía.
+                    Se ve la necesidad de capacitar a los servidores en cuanto a ética y
+                    cultura organizacional, ya que tiene como responsabilidad la estructura
+                    organizacional, La gestión integral del talento humano, La gestión de la
+                    información, La tecnología para el mejoramiento continuo de la prestación
+                    de servicios.
+                    <div>
+                        <button v-on:click="cerrar(1)">Cerrar</button>
+                    </div>
+                </div>
+                <div class="home-informacion-body-responsive">
+                    Se crea desde la Secretaría de Gestión Humana y Servicio a la Ciudadanía.
+                    Se ve la necesidad de capacitar a los servidores en cuanto a ética y
+                    cultura organizacional, ya que tiene como responsabilidad la estructura
+                    organizacional, La gestión integral del talento humano, La gestión de la
+                    información, La tecnología para el mejoramiento continuo de la prestación
+                    de servicios.
+                </div>
+                <div class="home-informacion-body-responsive-md">
                     Se crea desde la Secretaría de Gestión Humana y Servicio a la Ciudadanía.
                     Se ve la necesidad de capacitar a los servidores en cuanto a ética y
                     cultura organizacional, ya que tiene como responsabilidad la estructura
@@ -90,11 +126,33 @@
                         <button v-on:click="cerrar(2)">Cerrar</button>
                     </div>
                 </div>
+                <div class="home-informacion-body-responsive">
+                    Su misión se cimienta en tres aspectos: el SER (su formación),
+                    el HACER (su entrenamiento) y el SABER (su capacitación).
+                </div>
+                <div class="home-informacion-body-responsive-md">
+                    Su misión se cimienta en tres aspectos: el SER (su formación),
+                    el HACER (su entrenamiento) y el SABER (su capacitación).
+                    <div>
+                        <button v-on:click="cerrar(2)">Cerrar</button>
+                    </div>
+                </div>
                 <div class="home-informacion-button" v-on:click="MostrarInfoButton(3)">
                     <img src="../assets/img/modal-icon-4.png" alt="" class="mt-2 home-icon-informacion">
                     <p class="text-center mx-2 home-valor-texto-titulo">Programas <br> &nbsp;</p>
                 </div>
                 <div class="home-informacion-body">
+                    La plataforma cuenta con 13 secretarías Actualmente
+                    hay 64 programas de aprendizaje, 56 cursos y 8 diplomados.
+                    <div>
+                        <button v-on:click="cerrar(3)">Cerrar</button>
+                    </div>
+                </div>
+                <div class="home-informacion-body-responsive">
+                    La plataforma cuenta con 13 secretarías Actualmente
+                    hay 64 programas de aprendizaje, 56 cursos y 8 diplomados.
+                </div>
+                <div class="home-informacion-body-responsive-md">
                     La plataforma cuenta con 13 secretarías Actualmente
                     hay 64 programas de aprendizaje, 56 cursos y 8 diplomados.
                     <div>
@@ -111,13 +169,23 @@
                     un grupo de expertos en tecnología e innovación e implementada a
                     través de objetos virtuales de aprendizaje, tales como cartillas,
                     videojuegos, podcasts, cuentos y material educativo.
-
-                    Se encuentran en funcionamiento los simulacros de pruebas ICFES
-                    para los alumnos de grado 11 y saber pro para los alumnos de 9 y
-                    10 de todas las instituciones públicas. Durante la pandemia, la
-                    plataforma tomó más fuerza, gracias a la divulgación y promoción
-                    del aprendizaje ofrecido por las dependencias.el contenido de los cursos, la temática, metodología y duración
-                    del mismo.
+                    <div>
+                        <button v-on:click="cerrar(4)">Cerrar</button>
+                    </div>
+                </div>
+                <div class="home-informacion-body-responsive">
+                    A partir de mayo se reactivará en la plataforma la metodología
+                    PREA para la prevención del embarazo adolescente, construida por
+                    un grupo de expertos en tecnología e innovación e implementada a
+                    través de objetos virtuales de aprendizaje, tales como cartillas,
+                    videojuegos, podcasts, cuentos y material educativo.
+                </div>
+                <div class="home-informacion-body-responsive-md">
+                    A partir de mayo se reactivará en la plataforma la metodología
+                    PREA para la prevención del embarazo adolescente, construida por
+                    un grupo de expertos en tecnología e innovación e implementada a
+                    través de objetos virtuales de aprendizaje, tales como cartillas,
+                    videojuegos, podcasts, cuentos y material educativo.
                     <div>
                         <button v-on:click="cerrar(4)">Cerrar</button>
                     </div>
@@ -126,7 +194,7 @@
         </section>
         <!-- FIN Información adicional para el Conoce Más -->
         <!-- Footer -->
-        <div class="d-flex justify-content-between w-100 home-footer px-5">
+        <div class="d-flex justify-content-between w-100 home-footer px-0 px-md-5">
             <div class="d-flex home-footer-limit">
                 <img src="../assets/img/footer-logo.png" alt="" class="img-fluid home-footer-img">
             </div>
@@ -152,25 +220,53 @@ export default {
     },
     MostrarInfoButton (i) {
       const informacion = document.querySelectorAll('.home-informacion-body')
+      const informacionResponsive = document.querySelectorAll('.home-informacion-body-responsive-md')
       const button = document.querySelectorAll('.home-informacion-button')
-      for (let x = 0; x <= informacion.length; x++) {
-        if (x === i) {
-          informacion[i].style.animation = 'home-informacion-aparecer forwards 1s'
+      for (let x = 0; x < informacion.length; x++) {
+        if (screen.width < 1200 && screen.width > 560) {
+          if (x === i) {
+            informacionResponsive[i].style.margin = '20px 0'
+            informacionResponsive[i].style.width = '300px'
+            informacionResponsive[i].style.display = 'flex'
+            informacionResponsive[i].style.padding = '20px'
+            informacionResponsive[i].style.boxShadow = '0 0px 20px rgba(0, 0, 0, 0.20)'
+          }
+          if (x !== i) {
+            button[x].style.display = 'none'
+            informacionResponsive[x].style.display = 'none'
+          }
         }
-        if (x !== i) {
-          button[x].classList.add('d-none')
+        if (screen.width > 1200) {
+          if (x === i) {
+            informacion[i].style.animation = 'home-informacion-aparecer forwards 1s'
+          }
+          if (x !== i) {
+            button[x].style.display = 'none'
+          }
         }
       }
     },
     cerrar (i) {
       const button = document.querySelectorAll('.home-informacion-button')
+      const informacionResponsive = document.querySelectorAll('.home-informacion-body-responsive-md')
       const informacion = document.querySelectorAll('.home-informacion-body')
-      for (let x = 0; x <= informacion.length; x++) {
-        if (x === i) {
-          informacion[i].style.animation = ''
+      for (let x = 0; x < informacion.length; x++) {
+        if (screen.width < 1200 && screen.width > 560) {
+          if (x === i) {
+            informacionResponsive[i].style.display = 'none'
+          }
+          if (x !== i) {
+            button[x].style.display = 'flex'
+            informacionResponsive[x].style.display = 'none'
+          }
         }
-        if (x !== i) {
-          button[x].classList.remove('d-none')
+        if (screen.width > 1200) {
+          if (x === i) {
+            informacion[i].style.animation = ''
+          }
+          if (x !== i) {
+            button[x].style.display = 'flex'
+          }
         }
       }
     }
